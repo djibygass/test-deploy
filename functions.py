5d0754666c4502077b5e73d6b44da0cc3679f975
djibygass <gassamadjibril@outlook.com>
2024-06-26 14:35:19 +0200

    Set up CI pipeline with tests, linting, and Docker build

---
+++ functions.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        return "Can't divide by zero"
+    else:
+        return a / b
